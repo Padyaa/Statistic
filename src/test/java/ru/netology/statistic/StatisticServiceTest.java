ad1d22015c067680b7a48376bf276218b7a70c92
Padyaa <zehopa@mail.ru>
2022-08-11 19:01:19 +0300

    BRANCH100%

--- src/test/java/ru/netology/statistic/StatisticServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticServiceTest.java
@@ -17,4 +17,16 @@ public class StatisticServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void findMax1() {
+        StatisticService service = new StatisticService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 110, 11, 12};
+        long expected = 110;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
 }
